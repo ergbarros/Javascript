f058bebd40a35afeef7479761f00fdefb152898e
EDINALDO BARROS <115654611+ergbarros@users.noreply.github.com>
2023-11-13 21:52:54 -0300

    Add files via upload

---
+++ appflex/flex.js
@@ -0,0 +1,19 @@
+/**
+ * APP Flex
+ * @author Professor José de Assis
+ */
+
+function calcular() {
+    // variável etanol recebe o conteúdo da caixa de texto
+    let etanol = frmFlex.txtEtanol.value
+    let gasolina = frmFlex.txtGasolina.value
+    if (etanol < 0.7 * gasolina) {
+        document.getElementById("status").src = "icons/etanol.png"
+    } else {
+        document.getElementById("status").src = "icons/gasolina.png"
+    }
+}
+
+function limpar() {
+    document.getElementById("status").src = "icons/neutro.png"
+}
